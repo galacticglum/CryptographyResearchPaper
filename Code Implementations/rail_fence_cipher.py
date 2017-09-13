5cbf6b224cc2cba94f27d0791df4e8080f783141
GalacticGlum <nerftactic09@gmail.com>
2017-09-13 11:14:25 -0400

    Better rail fence cipher implementation.

--- Code Implementations/rail_fence_cipher.py
+++ Code Implementations/rail_fence_cipher.py
@@ -1,47 +1,44 @@
-import math
+def encrypt(plain_text, rails):
+    cipher_text = str()
+    railskip = max((rails - 1) * 2, 1) # 1 is special case for 1 rail
+    
+    for rail in range(rails):
+        railsymbol = rail
+        increase = railskip - 2 * rail
+        if rail == rails - 1:
+            increase = railskip
 
-def handle_rail_iteration(on_iteration_function, width, rail_num):
-    y = 0
-    direction = 0
-    for i in range(width):
-        on_iteration_function(y, i)
-        if y == 0:
-            direction = 1
-        elif y == rail_num - 1:
-            direction = -1
-            
-        y += direction
+        while railsymbol < len(plain_text):
+            cipher_text += plain_text[railsymbol]
+            railsymbol += increase
 
-def encrypt(plain_text, rail_num):
-    width = len(plain_text)
-    rows = [''] * rail_num
+            if rail != 0 and rail != rails - 1:
+                increase = railskip - increase
 
-    def iteration(y, i):
-        rows[y] += plain_text[i]
+    return cipher_text
 
-    handle_rail_iteration(iteration, width, rail_num)
-    return ''.join(rows)
-     
-def decrypt(cipher_text, rail_num):
-    width = len(cipher_text)
-    rail_fence = [[] for i in range(rail_num)]
-    handle_rail_iteration(lambda y, i: rail_fence[y].append(i), width, rail_num)
+def decrypt(cipher_text, rails):
+    plain_text = [''] * len(cipher_text)
+    cipher_index = 0
+    railskip = max((rails - 1) * 2, 1)  # 1 is special case for 1 rail
+    for rail in range(rails):
+        railsymbol = rail
+        increase = railskip - 2 * rail
 
-    ptr = 0
-    decrypted = [''] * width
+        if rail == rails - 1:
+            increase = railskip
 
-    for i in range(rail_num):
-        for j in range(len(rail_fence[i])):
-            decrypted[rail_fence[i][j]] = cipher_text[ptr]
-            ptr += 1
+        while railsymbol < len(plain_text):
+            plain_text[railsymbol] = cipher_text[cipher_index]
+            cipher_index += 1
+            railsymbol += increase
 
-    return ''.join(decrypted)
+            if rail != 0 and rail != rails - 1:
+                increase = railskip - increase
 
-key = int(input('How many rails should I encrypt with? '))
-plain_text = input('What should I encrypt? ')
+    return ''.join(plain_text)
 
-result = encrypt(plain_text, key)
-chunk_size = 5
-
-print('\nEncrypted message: '+' '.join([result[i:i+chunk_size] for i in range(0, len(plain_text), chunk_size)]))
-print('Decrypted message: ' + decrypt(result, key))
+rails = int(input('How many rails should I encrypt with?\n'))
+cipher_text = encrypt(input('What should I encrypt?\n'), rails)
+print('\nEncrypted: ' + cipher_text)
+print('Decrypted: ' + decrypt(cipher_text, rails))
